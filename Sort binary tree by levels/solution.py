d63ab35910f39e736b8842fdcf67596b7690c7e5
NazarDizhak <dizhak.pn@ucu.edu.ua>
2024-04-23 21:39:57 +0300

    Sort binary tree by levels

---
+++ Sort binary tree by levels/solution.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def tree_by_levels(root):
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+        result.append(node.value)
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+    return result
